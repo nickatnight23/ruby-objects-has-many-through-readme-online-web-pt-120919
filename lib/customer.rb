085d24bf82ebec8e239613e566b0477be7f519ce
Latricia Nickelberry <tricianickelberry@gmail.com>
2020-01-22 15:13:59 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,26 @@
 class Customer
-  
+  attr_accesor :name, :age
+
+@@all =[]  
+
+def initialize (name, age)
+  @name = name
+  @age =age
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+def meals
+  Meal.all.select do |meal|
+  meal.customer == self
+end
+
+def waiters
+  meals.map do |meal|
+  meal.waiter
+    
+def new_meal
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,25 @@
 class Waiter
+  attr_accessor :name, :yrs_experience
+  
+  @@all []
+  
+  def initialize (name, yrs_experence)
+    @name = name 
+    @yrs_experience = yrs_experience
+    @@all << self
+end
 
+def self.all
+  @@all
+  end
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self
+    end
+    
+    def best_tipper
+      best_tipped_meal = meals.max do |meal_a,meal_b|
+        meal_a.tip <=> meal_b.tip
+      end
 end
